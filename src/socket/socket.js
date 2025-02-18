fe8f5d11590a88f5c58db572cd84b9ef5ba88a9f
Omkar Rahi <omk.rahi@gmail.com>
2025-02-18 22:07:28 +0530

    socket change

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -6,6 +6,9 @@ export const connectSocket = (user) => {
   if (!user) return;
   const socket = io.connect(backendUrl, {
     query: { userId: user.id },
+    transports: ["websocket"],
+    withCredentials: true,
+    forceNew: true,
   });
 
   return socket;
